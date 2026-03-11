8cf8f63df912ebacdb2521f797c8163f7af1e390
afflorezc <afflorezc@gmail.com andresf.florezc@udea.edu.co>
2026-03-11 01:29:27 -0500

    restructured sql queries for testing complete classes with karate

--- src/test/resources/test-data/add-customer.sql
+++ src/test/resources/test-data/add-customer.sql
@@ -1,2 +1,4 @@
+DELETE FROM customer;
+ALTER TABLE customer AUTO_INCREMENT = 1;
 INSERT INTO customer (name, last_name, email, phone_number, document_type, document_number, status)
 VALUES ('Camilo', 'Perez', 'camilop@gmail.com', '3154238456', 'CEDULA_CIUDADANIA', '1057452245', 'Activo');
